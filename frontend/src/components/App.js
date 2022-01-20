2e81786dd64ac584b03555ef0675675dd34f20e8
Michael Ng <michaelngky@gmail.com>
2022-01-20 15:16:28 +0800

    changed max width

--- frontend/src/components/App.js
+++ frontend/src/components/App.js
@@ -8,7 +8,7 @@ function App() {
   return (
     <Container className='d-flex align-items-center justify-content-center'
       style={{ minHeight: '100vh' }}>
-      <div className='w-100' style={{ maxWidth: '400px' }}>
+      <div className='w-100' style={{ maxWidth: '1000px' }}>
         <Router>
           <Routes>
             <Route path='/signup' element={<Signup />} />
