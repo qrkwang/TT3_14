d31c027a81257dd9d307912a1f170e7c14cd5337
Michael Ng <michaelngky@gmail.com>
2022-01-20 15:08:53 +0800

    add homepage

--- frontend/src/components/Homepage.js
+++ frontend/src/components/Homepage.js
@@ -1,7 +1,14 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Container, Navbar, Nav, Card } from 'react-bootstrap';
+// import Axios from 'axios'
 
 function Homepage() {
+    const [listOfUsers, setListofUsers] = useState([{ name: 'Michael', age: 20 }]);
+
+    // useEffect(() => {
+    //     Axios.get('localhost:8080/post/getall')
+    // }, [])
+
     return (
         <Card>
             <Card.Body>
@@ -17,6 +24,16 @@ function Homepage() {
                         </Navbar.Collapse>
                     </Container>
                 </Navbar>
+                <Container>
+                    {listOfUsers.map((user) => {
+                        return (
+                            <div>
+                                <h2>Name: {user.name}</h2>
+                                <p>Age: {user.age}</p>
+                            </div>
+                        );
+                    })}
+                </Container>
             </Card.Body>
         </Card>
     )
